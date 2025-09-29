3abf03ea24edf8525b0f49609314aca8d02e454d
Yuri Zetoles <yurizetoles0123@gmail.com>
2025-09-29 18:47:50 -0400

    refactor: Remover componentes Banner e Coleta do arquivo Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,6 @@
-import Banner from "@/components/banner";
-import Coleta from "./coleta/page";
-
 export default function Home() {
   return (
     <div>
-      {/* Banner principal para Coleta */}
-      <Banner
-        tipoDemandaId="exemplo-coleta-id"
-        manualIcon="/trash-icon.svg"
-        manualTitulo="Coleta"
-        backgroundImage="/banner.png"
-        className="mb-8"
-      />
-
-      {/* Exemplo comentado para outros serviços */}
-      {/* 
-      <Banner 
-        tipoDemandaId="outro-servico-id"
-        manualIcon="/outro-icon.svg"
-        manualTitulo="Outro Serviço"
-        backgroundImage="/banner.png"
-        className="mb-8"
-      />
-      */}
-      <Coleta />
     </div>
   );
 }
